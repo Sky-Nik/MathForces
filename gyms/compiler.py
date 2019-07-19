88c2d9feb83391d5b4f04fe763e39066617d6f75
Sky-Nik <n.skybytskyi@gmail.com>
2019-07-19 14:26:43 +0300

    Recompiled gyms table

---
+++ gyms/compiler.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import json
+
+
+LOCAL_CATEGORY_NAMES_UPPERCASE = {
+  "a":   "Алгебра",
+  "c":   "Комбінаторика",
+  "g":   "Геометрія",
+  "n":   "Теорія чисел",
+  "o":   "Олімпіади",
+  "m":   "Різнобій",
+  "all": "Всі",
+}
+
+LOCAL_DIFFICULTY_NAMES_UPPERCASE = {
+  "ve":  "Дуже легко",
+  "e":   "Легко",
+  "m":   "Середнє",
+  "h":   "Складно",
+  "vh":  "Дуже складно",
+  "imo": "IMO™",  # this ™ is a trademark symbol
+  "all": "Всі",
+}
+
+
+if __name__ == '__main__':
+  compiled_gyms_table = ""
+  for file in filter(lambda _: os.path.splitext(_)[1] == '.json', os.listdir()):
+    gym = json.loads(open(file, 'r', encoding='utf-8').read())
+    assert gym['type'] == 'gym'
+    compiled_gyms_table += f'''
+              <tr сlass="gym_item" _cat="{gym['_cat']}" _grade="{gym['_grade']}" _diff="{gym['_diff']}" gym-id="{gym['id']}">
+                <td class="gym-source">{gym['source']}</td>
+                <td class="gym-category">{gym['_cat']}</td>
+                <td class="gym-topic">{gym['name']}: 
+                  <a href="/MathForces/gyms/{gym['pdf_link']}.pdf">[pdf]</a>,
+                  <a href="v/MathForces/gyms/{gym['id']}_t">[web]</a>,
+                  <span style="color: gray; font-size: smaller;">{gym['_num_probs']}&nbsp;задач{'і' if 2 <= gym['_num_probs'] <= 4 else ''}</span></td>
+                <td class="gym-date">{gym['_date']}</td>
+                <td class="gym-grade">{gym['_grade']}</td>
+                <td clas="gym-difficulty">{gym['_diff']}</td>
+              </tr>'''
+  open('compiled_gyms_table.html', 'w', encoding='utf-8').write(compiled_gyms_table)
+  # autoupdate contets table on GitHub Pages:
+  os.system('git add . && git commit -m "Recompiled gyms table" && git push')
--- gyms/filter.js
+++ gyms/filter.js
@@ -4,19 +4,19 @@ var CURRENT_DIFFICULTY = "all";
 
 var LOCAL_CATEGORY_NAMES_LOWERCASE = {
   "a": "алгебра",
-  "c": "комбинаторика",
-  "g": "геометрия",
-  "n": "теория чисел",
-  "o": "олимпиады",
-  "m": "разнобой", // mash-up, mixture
+  "c": "комбінаторика",
+  "g": "геометрія",
+  "n": "теорія чисел",
+  "o": "олімпіади",
+  "m": "різнобій", // mash-up, mixture
 };
 
 var LOCAL_DIFFICULTY_NAMES_LOWERCASE = {
-  "ve":  "оч. легко",
+  "ve":  "дуже легко",
   "e":   "легко",
-  "m":   "средне",
-  "h":   "сложно",
-  "vh":  "оч. сложно",
+  "m":   "середнє",
+  "h":   "складно",
+  "vh":  "дуже складно",
   "imo": "IMO™", // this ™ is a trademark symbol
 };
 
@@ -25,28 +25,28 @@ function update_params(param_type, param_value) {
   if (param_type === "CATEGORY") {
     CURRENT_CATEGORY = param_value;
     if (CURRENT_CATEGORY === "all") {
-      document.getElementById("FILTER_BY_CATEGORY_BUTTON").textContent = "Выбор по тематике";
+      document.getElementById("FILTER_BY_CATEGORY_BUTTON").textContent = "Вибір за тематикою";
     } else {
       document.getElementById("FILTER_BY_CATEGORY_BUTTON").textContent = "Тематика: " + LOCAL_CATEGORY_NAMES_LOWERCASE[CURRENT_CATEGORY];
     }
   } else if (param_type === "GRADE") {
     CURRENT_GRADE = param_value;
     if (CURRENT_GRADE === "all") {
-      document.getElementById("FILTER_BY_GRADE_BUTTON").textContent = "Выбор по классу";
+      document.getElementById("FILTER_BY_GRADE_BUTTON").textContent = "Вибір за класом";
     } else {
-      document.getElementById("FILTER_BY_GRADE_BUTTON").textContent = "Класс: " + CURRENT_GRADE;
+      document.getElementById("FILTER_BY_GRADE_BUTTON").textContent = "Клас: " + CURRENT_GRADE;
     }
   } else if (param_type === "DIFFICULTY") {
     CURRENT_DIFFICULTY = param_value;
     if (CURRENT_DIFFICULTY === "all") {
-      document.getElementById("FILTER_BY_DIFFICULTY_BUTTON").textContent = "Выбор по сложности";
+      document.getElementById("FILTER_BY_DIFFICULTY_BUTTON").textContent = "Вибір за складністю";
     } else {
-      document.getElementById("FILTER_BY_DIFFICULTY_BUTTON").textContent = "Сложность: " + LOCAL_DIFFICULTY_NAMES_LOWERCASE[CURRENT_DIFFICULTY];
+      document.getElementById("FILTER_BY_DIFFICULTY_BUTTON").textContent = "Складність: " + LOCAL_DIFFICULTY_NAMES_LOWERCASE[CURRENT_DIFFICULTY];
     }
   }
 
-  // update finder_items
-  var elements = document.getElementsByClassName("finder_item");
+  // update gym_items
+  var elements = document.getElementsByClassName("gym_item");
   var k = 0; // matches counter
   for (var i = 0; i < elements.length; ++i) {
     if ((CURRENT_CATEGORY   === "all" || elements[i].getAttribute('_cat')   === CURRENT_CATEGORY) &&
@@ -66,11 +66,60 @@ function update_params(param_type, param_value) {
   }
 
   // hide table if no problemsets found
-  document.getElementById("classes_found").textContent = "Занятий: " + k;
+  document.getElementById("classes_found").textContent = "Занять: " + k;
   if (k === 0) {
-    document.getElementById("finder_table").style.display = "none";
+    document.getElementById("gyms_table").style.display = "none";
   } else {
-    document.getElementById("finder_table").style.display = "table";
+    document.getElementById("gyms_table").style.display = "table";
   }
+};
 
+var LOCAL_CATEGORY_NAMES_UPPERCASE = {
+  "a":   "Алгебра",
+  "c":   "Комбінаторика",
+  "g":   "Геометрія",
+  "n":   "Теорія чисел",
+  "o":   "Олімпіади",
+  "m":   "Різнобій",
+  "all": "Всі",
+};
+
+var LOCAL_GRADE_NAMES_UPPERCASE = {
+  "6":   "6",
+  "7":   "7",
+  "8":   "8",
+  "9":   "9",
+  "10":  "10",
+  "11":  "11",
+  "all": "Все",
+};
+
+var LOCAL_DIFFICULTY_NAMES_UPPERCASE = {
+  "ve":  "Дуже легко",
+  "e":   "Легко",
+  "m":   "Середнє",
+  "h":   "Складно",
+  "vh":  "Дуже складно",
+  "imo": "IMO™", // this ™ is a trademark symbol
+  "all": "Всі",
+};
+
+function compile_filters() {
+  var s = "";
+  for (var key in LOCAL_CATEGORY_NAMES_UPPERCASE) {
+    s += '<p onclick="update_params(\'CATEGORY\', \'' + key + '\');">' + LOCAL_CATEGORY_NAMES_UPPERCASE[key] + '</p>';
+  }
+  document.getElementById("FILTER_BY_CATEGORY").innerHTML += '<div class="dropdown-content">' + s + '</div>';
+
+  s = "";
+  for (var key in LOCAL_GRADE_NAMES_UPPERCASE) {
+    s += '<p onclick="update_params(\'GRADE\', \'' + key + '\');">' + LOCAL_GRADE_NAMES_UPPERCASE[key] + '</p>';
+  }
+  document.getElementById("FILTER_BY_GRADE").innerHTML += '<div class="dropdown-content">' + s + '</div>';
+
+  s = "";
+  for (var key in LOCAL_DIFFICULTY_NAMES_UPPERCASE) {
+    s += '<p onclick="update_params(\'DIFFICULTY\', \'' + key + '\');">' + LOCAL_DIFFICULTY_NAMES_UPPERCASE[key] + '</p>';
+  }
+  document.getElementById("FILTER_BY_DIFFICULTY").innerHTML += '<div class="dropdown-content">' + s + '</div>';
 };
