b65017eb7ac9804967bea374c98d26e38d2dad29
Sky-Nik <n.skybytskyi@gmail.com>
2019-07-19 18:00:21 +0300

    Recompiled contests table

--- contests/compiler.py
+++ contests/compiler.py
@@ -7,13 +7,18 @@ if __name__ == '__main__':
   for file in filter(lambda _: os.path.splitext(_)[1] == '.json', os.listdir()):
     contest = json.loads(open(file, 'r', encoding='utf-8').read())
     assert contest['type'] == 'contest'
-    compiled_contests_table += f'''
-              <tr contest-id="{contest['id']}">
+    compiled_contests_table += f'''              <tr contest-id="{contest['id']}">
                 <td class="contest-number-column"><a href="/MathForces/contests/{contest['id']}_t.html">{contest['id']}</a></td>
                 <td class="contest-name-column"><a href="/MathForces/contests/{contest['id']}_t.html">{contest['name']}</a></td>
                 <td class="contest-start-time-column">{contest['start_time'].replace(', ', '<br>')}</td>
                 <td class="contest-duration-column">{contest['duration'].zfill(5)}</td>
-              </tr>'''
-  open('compiled_contests_table.html', 'w', encoding='utf-8').write(compiled_contests_table)
+              </tr>
+'''
+  # update index:
+  index_lines = open('index.html', 'r', encoding='utf-8').readlines()
+  table_body_start = index_lines.index('            <tbody id="contests-table-body">\n')
+  table_body_end = index_lines.index('            </tbody>\n')
+  index_lines = index_lines[:table_body_start + 1] + [compiled_contests_table, ] + index_lines[table_body_end:]
+  open('index.html', 'w', encoding='utf-8').writelines(index_lines)
   # autoupdate contets table on GitHub Pages:
   os.system('git add . && git commit -m "Recompiled contests table" && git push')
